11f5f893124344d460c04c333ac97b184bf3424f
Quoc Le <quocle@Quocs-MacBook-Pro.local>
2019-08-06 17:30:24 +0700

    initial

---
+++ genesis/hard_start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DATADIR="./blockchain"
+
+if [ ! -d $DATADIR ]; then
+  mkdir -p $DATADIR;
+fi
+
+nodeos \
+--signature-provider EOS6pcdgzFGRXquEYJrrn4sJxGw5bGPZABiqWpyi1jRZp4PBUd6ud=KEY:5KWWqPKUxj1PN6vtJrssd2pbbKFk2m4WZLMHmf4Nowov5Xu9pYU \
+--plugin eosio::producer_plugin \
+--plugin eosio::chain_api_plugin \
+--plugin eosio::http_plugin \
+--plugin eosio::history_api_plugin \
+--plugin eosio::history_plugin \
+--data-dir $DATADIR"/data" \
+--blocks-dir $DATADIR"/blocks" \
+--config-dir $DATADIR"/config" \
+--producer-name eosio \
+--http-server-address 0.0.0.0:8888 \
+--p2p-listen-endpoint 0.0.0.0:9010 \
+--access-control-allow-origin=* \
+--contracts-console \
+--http-validate-host=false \
+--verbose-http-errors \
+--enable-stale-production \
+--p2p-peer-address localhost:9011 \
+--p2p-peer-address localhost:9012 \
+--p2p-peer-address localhost:9013 \
+--hard-replay-blockchain \
+>> $DATADIR"/nodeos.log" 2>&1 & \
+echo $! > $DATADIR"/eosd.pid"
+
