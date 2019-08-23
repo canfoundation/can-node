64bf3b1a54467b9e1848a7104f9da1a65aa44434
Quoc Le <songmai108@gmail.com>
2019-08-23 10:33:40 +0700

    add pubkey prikey varibe

--- genesis/genesis_start.sh
+++ genesis/genesis_start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 DATADIR="./blockchain"
 
+BPACCOUNT=
+PUBKEY=
+PRIKEY=
+
 if [ ! -d $DATADIR ]; then
   mkdir -p $DATADIR;
 fi
 
 nodeos \
 --genesis-json $DATADIR"/../../genesis.json" \
---signature-provider EOS6pcdgzFGRXquEYJrrn4sJxGw5bGPZABiqWpyi1jRZp4PBUd6ud=KEY:5KWWqPKUxj1PN6vtJrssd2pbbKFk2m4WZLMHmf4Nowov5Xu9pYU \
+--signature-provider $PUBKEY=KEY:$PRIKEY \
 --plugin eosio::producer_plugin \
 --plugin eosio::producer_api_plugin \
 --plugin eosio::chain_api_plugin \
@@ -17,7 +21,7 @@ nodeos \
 --data-dir $DATADIR"/data" \
 --blocks-dir $DATADIR"/blocks" \
 --config-dir $DATADIR"/config" \
---producer-name eosio \
+--producer-name $BPACCOUNT \
 --http-server-address 0.0.0.0:8888 \
 --p2p-listen-endpoint 0.0.0.0:9010 \
 --access-control-allow-origin=* \
